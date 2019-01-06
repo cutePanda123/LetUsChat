95107a2870167c8e08a50bd9ef9fb8ecbfe5fd0d
peterliUmich <longhanli123@gmail.com>
2019-01-06 13:41:34 -0800

    Added docker support

--- LetUsChat/Program.cs
+++ LetUsChat/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -19,7 +20,16 @@ namespace LetUsChat
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+            .UseKestrel(options =>
+            {
+                options.Listen(IPAddress.Loopback, 44342);  // http:localhost:5000
+                options.Listen(IPAddress.Any, 80);         // http:*:80
+                options.Listen(IPAddress.Loopback, 443, listenOptions =>
+                {
+                    listenOptions.UseHttps("certificate.pfx", "password");
+                });
+            })
+            .UseStartup<Startup>()
+            .Build();
     }
 }
